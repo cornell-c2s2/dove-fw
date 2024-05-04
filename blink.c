c2a660fad33fb1989c49a672dc12ffda5be902db
ec798 <ec798@cornell.edu>
2024-05-04 12:24:05 -0400

    Moved blink.c

--- blink.c
+++ blink.c
@@ -1,11 +1,11 @@
-#include "../board_utils/defs.h"
-#include "../include/src.h"
-#include "../include/fake.h"
-#include "../include/mem_array.h"
+#include "defs.h"
+#include "src.h"
+#include "fake.h"
+#include "mem_array.h"
 
 // define number of samples wanted for data and kernel
 int num_samples = 100;
-int kernel_length = 20; 
+int kernel_length = 20;
 // #include "../local_defs.h"
 // #include "../stub.c"
 
@@ -138,27 +138,50 @@ void delay(const int d)
 
 void blink(bool on)
 {
-    if (on) {
-    reg_gpio_out = 0; // ON
-    reg_mprj_datah = 0x0000003f;
-    reg_mprj_datal = 0xffffffff;
+    if (on)
+    {
+        reg_gpio_out = 0; // ON
+        reg_mprj_datah = 0x0000003f;
+        reg_mprj_datal = 0xffffffff;
 
-    delay(8000000);
-    } else {
+        delay(8000000);
+    }
+    else
+    {
 
-    reg_gpio_out = 1; // OFF
-    reg_mprj_datal = 0x00000000;
-    reg_mprj_datah = 0x00000000;
+        reg_gpio_out = 1; // OFF
+        reg_mprj_datal = 0x00000000;
+        reg_mprj_datah = 0x00000000;
 
-    delay(8000000);
+        delay(8000000);
     }
 }
 
-
 int main()
 {
-    // int i, j, k;
+    // Allocate memory, about 2kb
+    // Entries 0 to num_samples - 1 contain data with noise (AKA actual data)
+    // Entries num_samples to kernel_length - 1 contain the template signal to match with
+    // Entries kernel_length to num_samples + kernel_length - 2 contain the result of the matched filter
+    // It is the duty of the caller to make sure there is enough space in the ptr.
+    char *ptr = mem_arr_alloc();
 
+    // put samples in ptr
+    get_samples(ptr, num_samples, kernel_length);
+
+    // Note: unsigned chars goes from 0 to 255, signed goes from -127 to 127
+    //  We will used signed chars to get real data from bird_1.h and bird_2.h
+
+    // Use matched filter
+    new_matched_filter(ptr, num_samples, kernel_length);
+    // matched_filter(fake_samples, fake_size, kernel_samples, kernel_size, filtered);
+
+    char filtered_size = num_samples + kernel_length - 1;
+    // Starting index for putting matched filter results
+    int start = num_samples + kernel_length;
+
+// int i, j, k;
+#ifdef RISCV_BOARD
     reg_gpio_mode1 = 1;
     reg_gpio_mode0 = 0;
     reg_gpio_ien = 1;
@@ -244,33 +267,25 @@ int main()
     // delay(5000000);
     // }
 
-    // Allocate memory, about 2kb
-    // Entries 0 to num_samples - 1 contain data with noise (AKA actual data)
-    // Entries num_samples to kernel_length - 1 contain the template signal to match with
-    // Entries kernel_length to num_samples + kernel_length - 2 contain the result of the matched filter
-    // It is the duty of the caller to make sure there is enough space in the ptr. 
-    char *ptr = mem_arr_alloc();
-
-    //put samples in ptr
-    get_samples(ptr, num_samples, kernel_length); 
-
-    //Note: unsigned chars goes from 0 to 255, signed goes from -127 to 127
-    // We will used signed chars to get real data from bird_1.h and bird_2.h
-
-    // Use matched filter
-    new_matched_filter(ptr,num_samples,kernel_length);
-    // matched_filter(fake_samples, fake_size, kernel_samples, kernel_size, filtered);
-
-    char filtered_size = num_samples + kernel_length - 1;
-    // Starting index for putting matched filter results
-    int start = num_samples + kernel_length;
-
+    // If on board blink
     for (int i = 0; i < filtered_size; i++)
     {
-    
-        blink(filtered[start + i] > 10);
-        
+
+        blink(ptr[start + i] != 0);
+
         delay(8000000);
     }
     return 0;
+#else
+    // If not on board we print values!
+    for (int i = 0; i < filtered_size; i++)
+    {
+        int num = ptr[start + i];
+        if (num != 0)
+        {
+            printf("The value at entry %d is %d\n", start + i, num);
+        }
+    }
+    return 0;
+#endif
 }
