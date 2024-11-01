80ffa726d18ec4cce31d8f0a96af67e3699a57e5
eliascxstro <ec798@cornell.edu>
2024-11-01 16:41:23 -0400

    uart test stuff

---
+++ riscv-apps/uart_poll.c
@@ -0,0 +1,119 @@
+#include "defs.h"
+#include "csr.h"
+
+void delay(const int d)
+{
+
+  /* Configure timer for a single-shot countdown */
+  reg_timer0_config = 0;
+  reg_timer0_data = d;
+  reg_timer0_config = 1;
+
+  // Loop, waiting for value to reach zero
+  reg_timer0_update = 1; // latch current value
+  while (reg_timer0_value > 0)
+  {
+    reg_timer0_update = 1;
+  }
+}
+
+void blink(int on)
+{
+  if (on)
+  {
+    reg_gpio_out = 0; // ON
+    reg_mprj_datah = 0x0000003f;
+    reg_mprj_datal = 0xffffffff;
+
+    delay(8000000);
+  }
+  else
+  {
+
+    reg_gpio_out = 1; // OFF
+    reg_mprj_datal = 0x00000000;
+    reg_mprj_datah = 0x00000000;
+
+    delay(8000000);
+  }
+}
+
+int main()
+{
+  reg_gpio_mode1 = 1;
+  reg_gpio_mode0 = 0;
+  reg_gpio_ien = 1;
+  reg_gpio_oe = 1;
+
+  reg_mprj_io_37 = GPIO_MODE_MGMT_STD_OUTPUT;
+  reg_mprj_io_36 = GPIO_MODE_MGMT_STD_OUTPUT;
+  reg_mprj_io_35 = GPIO_MODE_MGMT_STD_OUTPUT;
+  reg_mprj_io_34 = GPIO_MODE_MGMT_STD_OUTPUT;
+  reg_mprj_io_33 = GPIO_MODE_MGMT_STD_OUTPUT;
+  reg_mprj_io_32 = GPIO_MODE_MGMT_STD_OUTPUT;
+  reg_mprj_io_31 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
+  reg_mprj_io_30 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
+  reg_mprj_io_29 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
+  reg_mprj_io_28 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
+  reg_mprj_io_27 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
+  reg_mprj_io_26 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
+  reg_mprj_io_25 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
+  reg_mprj_io_24 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
+  reg_mprj_io_23 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
+  reg_mprj_io_22 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
+  reg_mprj_io_21 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
+  reg_mprj_io_20 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
+  reg_mprj_io_19 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
+  reg_mprj_io_18 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
+  reg_mprj_io_17 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
+  reg_mprj_io_16 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
+  reg_mprj_io_15 = GPIO_MODE_MGMT_STD_OUTPUT;
+  reg_mprj_io_14 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
+  reg_mprj_io_13 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
+  reg_mprj_io_12 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
+  reg_mprj_io_11 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
+  reg_mprj_io_10 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
+  reg_mprj_io_9 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
+  reg_mprj_io_8 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
+  reg_mprj_io_7 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
+
+  // UART pis
+  reg_mprj_io_6 = GPIO_MODE_MGMT_STD_OUTPUT;       // UART Tx
+  reg_mprj_io_5 = GPIO_MODE_MGMT_STD_INPUT_NOPULL; // UART Rx
+
+  /* Lowest 5 GPIOs need to be set this way in order to	*/
+  /* access the housekeeping SPI at run-time.  Do not change	*/
+  /* them unless absolutely necessary.			*/
+
+  reg_mprj_io_4 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
+  reg_mprj_io_3 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
+  reg_mprj_io_2 = GPIO_MODE_MGMT_STD_INPUT_NOPULL;
+  reg_mprj_io_1 = GPIO_MODE_MGMT_STD_OUTPUT;
+
+  // Initiate the serial transfer
+  reg_mprj_xfer = 1;
+  while (reg_mprj_xfer == 1)
+    ;
+
+  // Enable uart
+  reg_uart_enable = 1;
+  while (1)
+  {
+    // Keep spinning till we get a signal
+    while (uart_rxempty_read())
+      ;
+
+    uint32_t num = uart_rxtx_read();
+    if (num % 2 == 0)
+    {
+      // Set 12 high, 13 low
+      reg_mprj_datal = 0x00000fff;
+    }
+    else
+    {
+      // Set 13 high, 12 low
+      reg_mprj_datal = 0xfffff000;
+    }
+  }
+  return 0;
+}
